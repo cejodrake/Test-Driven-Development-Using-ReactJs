f99cc0533ad26276ea0a0c05125b85a7589ca9e4
Juan Carlos Calix <31570936+JuanCarlosSoftwareDeveloper@users.noreply.github.com>
2019-09-14 21:24:53 -0700

    Configuration Initial

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+
+class Header extends Component {
+
+    render() {
+        return (
+            <div className="Header">
+                <p>
+                    this is the Header
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Header;
---
+++ src/containers/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+import '../App.css';
+import Header from '../components/Header';
+
+
+class App extends Component {
+
+    render() {
+        return (
+            <div className="App">
+                <Header />
+                <p className="App-intro">
+                    to test
+                </p>
+            </div>
+        );
+    }
+}
+export default App;
