09e7d94ece466c9ca0f1445fafada34775f4b643
Juan Carlos Calix <31570936+JuanCarlosSoftwareDeveloper@users.noreply.github.com>
2019-09-15 19:24:20 -0700

    Test Component Map and Button

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Button from '../Button';
 
-it('renders without crashing', () => {
-    let mountedButton = shallow(<Button />);
+describe('Button', () => {
+    it('renders without crashing', () => {
+        let mountedButton = shallow(<Button />);
+    })
+
 
 
 });
--- src/containers/__tests__/StoreLocator.test.js
+++ src/containers/__tests__/StoreLocator.test.js
@@ -14,6 +14,16 @@ describe('StoreLocator', () => {
     it('renders a header', () => {
         const header = mountedStoreLocator.find('Header');
         expect(header.length).toBe(1);
-    })
+    });
+    it('renders  two buttons', () => {
+        const buttons = mountedStoreLocator.find('Button');
+        expect(buttons.length).toBe(2);
+    });
+    it('render a map', () => {
+        const map = mountedStoreLocator.find('Map');
+        expect(map.length).toBe(1);
+    });
+
+
 })
 
